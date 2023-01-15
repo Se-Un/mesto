4ea404e5ee7d637aa383e164c3196e162fedc0d7
Kim Se-Un <kseun1949@gmail.com>
2023-01-15 19:17:44 +0300

    feat: добавить файл -nojekyll. оформить файл readme

--- blocks/popup/popup.js
+++ blocks/popup/popup.js
@@ -1,28 +1,28 @@
 'use strict'
-
+/*получить элементы блока profile*/
 let fullName = document.querySelector('.profile__full-name');
 let profileDesc = document.querySelector('.profile__description');
 let editButton = document.querySelector('.profile__edit-button');
 let addButton = document.querySelector('.profile__add-button');
-
+/*получить элементы блока popup*/
 let popup = document.querySelector('.popup');
 let buttonClose = popup.querySelector('.popup__button-close');
 let formElement = popup.querySelector('.popup__form');
 let nameInput = formElement.querySelector('.popup__fullname');
 let jobInput = formElement.querySelector('.popup__description');
-
+/*воспроизвести функцию открытия popup */
 function callPopup() {
     popup.classList.add('popup_opened');
 }
 editButton.addEventListener('click', callPopup);
-
+/*воспроизвести функцию закрытия popup*/
 function closePopup() {
   popup.classList.remove('popup_opened');
-  nameInput.value = fullName.textContent;
-  jobInput.value = profileDesc.textContent;
+  nameInput.value = fullName.getAttribute('placeholder');
+  jobInput.value = profileDesc.getAttribute('placeholder');
 }
 buttonClose.addEventListener('click', closePopup);
-
+/*воспроизвести функцию сохранения popup*/
 function handleFormSubmit(evt) {
   evt.preventDefault();
 
@@ -30,5 +30,4 @@ function handleFormSubmit(evt) {
   fullName.textContent = nameInput.value;
   profileDesc.textContent = jobInput.value;
 }
-
 formElement.addEventListener('submit', handleFormSubmit);
