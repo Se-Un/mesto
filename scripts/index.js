71d1190999ceec0b427e0a4b2354d621d3d012ba
Kim Se-Un <kseun1949@gmail.com>
2023-01-29 15:06:31 +0300

    feat: добавить изменения в html и js

--- scripts/index.js
+++ scripts/index.js
@@ -1,33 +1,79 @@
 'use strict'
+/*создать массив */
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+]; 
 /*получить элементы блока profile*/
-let fullName = document.querySelector('.profile__full-name');
-let profileDesc = document.querySelector('.profile__description');
-let editButton = document.querySelector('.profile__edit-button');
+const fullName = document.querySelector('.profile__full-name');
+const infoJob = document.querySelector('.profile__description');
+const editButton = document.querySelector('.profile__edit-button');
 /*получить элементы блока popup*/
-let popup = document.querySelector('.popup');
-let buttonClose = popup.querySelector('.popup__button-close');
-let formElement = popup.querySelector('.popup__form');
-let nameInput = formElement.querySelector('.popup__input_value_name');
-let jobInput = formElement.querySelector('.popup__input_value_job');
+const popup = document.querySelector('.popup');
+const buttonClose = popup.querySelector('.popup__button-close');
+const formElement = popup.querySelector('.popup__form');
+const nameInput = formElement.querySelector('.popup__input_value_name');
+const jobInput = formElement.querySelector('.popup__input_value_job');
+/*получить елементы блока elements*/
+const elements = document.querySelector('.elements');
+/*создать функцию для popup секции profile*/
+function callPopupProfile() {
 /*воспроизвести функцию открытия popup */
-function callPopup() {
+  const openPopup = () => {
     popup.classList.add('popup_opened');
     nameInput.value = fullName.textContent;
-    jobInput.value = profileDesc.textContent;
-}
+    jobInput.value = infoJob.textContent;
+  }
 /*воспроизвести функцию закрытия popup*/
-function closePopup() {
-  popup.classList.remove('popup_opened');
-}
+  const closePopup = () => {
+    popup.classList.remove('popup_opened');
+  }
 /*воспроизвести функцию сохранения popup*/
-function handleFormSubmit(evt) {
-  evt.preventDefault();
+  const handleFormSubmit = (evt) => {
+    evt.preventDefault();
 
-  closePopup();
-  fullName.textContent = nameInput.value;
-  profileDesc.textContent = jobInput.value;
-}
+    closePopup();
+    fullName.textContent = nameInput.value;
+    infoJob.textContent = jobInput.value;
+  }
 /*прослушивать события*/
-editButton.addEventListener('click', callPopup);
-buttonClose.addEventListener('click', closePopup);
-formElement.addEventListener('submit', handleFormSubmit);
+  editButton.addEventListener('click', openPopup);
+  buttonClose.addEventListener('click', closePopup);
+  formElement.addEventListener('submit', handleFormSubmit);
+
+}
+callPopupProfile();
+
+function suppElement(initialCards) {
+  
+    for(let i = 0; i < initialCards.length; i += 1) {
+      const elementTemplate = document.querySelector('#element-template').content;  
+      const cardElements = elementTemplate.querySelector('.element').cloneNode(true);
+      cardElements.querySelector('.element__image').src = initialCards[i].link;
+      cardElements.querySelector('.element__title').textContent = initialCards[i].name;
+      elements.append(cardElements);
+  }
+}
+suppElement(initialCards);
