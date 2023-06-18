61f5ffda0c15b03b9e4e7b7a38f790eb7a4b18bb
Kim Se-Un <kseun1949@gmail.com>
2023-06-18 19:02:33 +0300

    fix: исправить замечания по структуре проекта

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,14 +12,14 @@ import {
   popupScalePicture,
   initialCards,
   config
- } from './scripts/utils/constants.js';
-import  Card  from './scripts/components/Card.js';
-import  FormValidator  from './scripts/components/FormValidator.js';
-import  Section  from './scripts/components/Section.js';
-import Popup from './scripts/components/Popup.js';
-import PopupWithImage from './scripts/components/PopupWithImage.js';
-import PopupWithForm from './scripts/components/PopupWithForm.js';
-import UserInfo from './scripts/components/UserInfo.js';
+ } from '../scripts/utils/constants.js';
+import  Card  from '../scripts/components/Card.js';
+import  FormValidator  from '../scripts/components/FormValidator.js';
+import  Section  from '../scripts/components/Section.js';
+import Popup from '../scripts/components/Popup.js';
+import PopupWithImage from '../scripts/components/PopupWithImage.js';
+import PopupWithForm from '../scripts/components/PopupWithForm.js';
+import UserInfo from '../scripts/components/UserInfo.js';
 // объявить экземпляры класса Popup для открытия и закрытия попапов на странице
 const profileStatePopup = new Popup(profilePopup);
 const cardStatePopup = new Popup(cardPopup);
